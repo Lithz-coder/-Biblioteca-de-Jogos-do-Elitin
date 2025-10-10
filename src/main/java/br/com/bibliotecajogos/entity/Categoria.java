efb31560863a0cf171919f7570af5897c4c195ff
Eliton Carvalho <Eliton.rcj@gmail.com>
2025-10-10 19:54:34 -0300

    Update Categoria.java

--- src/main/java/br/com/bibliotecajogos/entity/Categoria.java
+++ src/main/java/br/com/bibliotecajogos/entity/Categoria.java
@@ -4,8 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import java.util.List;
 
 @Entity
 public class Categoria {
@@ -16,36 +14,17 @@ public class Categoria {
 
     private String nome;
 
-    @OneToMany(mappedBy = "categoria")
-    private List<Jogo> jogos;
+    // Construtor vazio
+    public Categoria() {}
 
-    // Construtor vazio (essencial para o JPA/Hibernate)
-    public Categoria() {
-    }
-
-    // --- Getters e Setters ---
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
+    // Construtor com nome
+    public Categoria(String nome) {
         this.nome = nome;
     }
 
-    public List<Jogo> getJogos() {
-        return jogos;
-    }
-
-    public void setJogos(List<Jogo> jogos) {
-        this.jogos = jogos;
-    }
+    // Getters e Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
 }
